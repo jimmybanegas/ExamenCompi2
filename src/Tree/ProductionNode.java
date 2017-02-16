0965ad811e8946f9d8201cc3eebb8b3fe4829e2b
Jimmy Ramos <jimmy.ramos@unitec.edu>
2017-02-15 20:21:37 -0600

    init project

---
+++ src/Tree/ProductionNode.java
@@ -0,0 +1,7 @@
+package Tree;
+
+/**
+ * Created by Jimmy Ramos on 15-Feb-17.
+ */
+public class ProductionNode extends  StatementNode {
+}
---
+++ src/Tree/StatementNode.java
@@ -0,0 +1,7 @@
+package Tree;
+
+/**
+ * Created by Jimmy Ramos on 15-Feb-17.
+ */
+public abstract class StatementNode {
+}
